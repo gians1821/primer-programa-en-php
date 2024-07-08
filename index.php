49ea4865d77af1b3de64251f1f774f6968924ea0
gians1821 <gian.samana.1801@gmail.com>
2024-07-07 23:48:40 -0500

    feat: operador nuevo

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ echo "=======================================================\n";
 echo "OPCIÓN 1. Ingresar productos\n";
 
 $name = readline("Ingresar nombre del producto: ");
+$quantity = readline("Ingresar la cantidad: ");
 $price = readline("Ingresar precio unitario del producto: ");
 
 // RESUMEN FINAL
@@ -14,4 +15,6 @@ echo "=============================\n";
 echo "========= RESUMEN ===========\n";
 echo "=============================\n";
 echo "Nombre del producto: $name\n";
+echo "Cantidad del producto: $quantity unidades\n";
 echo "Precio unitario del producto: $price soles\n";
+echo "Precio total del producto: " . ($quantity * $price) . " soles\n";
